9da61bcc9824bb4cea8dd711b922b81b20fd2a49
Marco Bartolini <marco.bartolini@gmail.com>
2015-01-28 14:56:56 +0100

    added screen package

--- bash/yum.sh
+++ bash/yum.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "configuring yum and installing packages"
 yum install -y epel-release
-yum install -y vim-enhanced subversion python26 python26-devel git jpackage-utils java-1.6.0-openjdk supervisor cfitsio gcc-c++ cmake ncurses ncurses-devel
+yum install -y vim-enhanced subversion python26 python26-devel git jpackage-utils java-1.6.0-openjdk supervisor cfitsio gcc-c++ cmake ncurses ncurses-devel screen
