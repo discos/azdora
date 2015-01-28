6d573f4e7fd6edecd6b71e1695711a7d17c12f5c
Marco Bartolini <marco.bartolini@gmail.com>
2015-01-28 17:12:30 +0100

    jenkins installation

--- bash/yum.sh
+++ bash/yum.sh
@@ -5,3 +5,10 @@ yum install -y vim-enhanced subversion python26 python26-devel \
                git jpackage-utils java-1.6.0-openjdk java-1.6.0-openjdk-devel \ 
                supervisor cfitsio gcc-c++ cmake ncurses ncurses-devel screen \
                expat expat-devel
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+cd /tmp
+wget https://jenkins-ci.org/redhat/jenkins-ci.org.key
+rpm --import jenkins-ci.org.key
+rm jenkins-ci.org.key
+yum install -y jenkins
+
