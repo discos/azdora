0803e9e189479a685bb5b4e68a7a59a68b747c8b
Marco Bartolini <marco.bartolini@gmail.com>
2015-02-02 17:17:39 +0100

    first jenkins customization

--- bash/jenkins.sh
+++ bash/jenkins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp files/ets.sysconfig.jenkins /etc/sysconfig/jenkins
+cp /vagrant/files/etc.sysconfig.jenkins /etc/sysconfig/jenkins
+rm -rf /var/lib/jenkins
+tar xzfp /vagrant/files/var.lib.jenkins -C /var/lib
 chown -R manager:acs /var/lib/jenkins
 chown -R manager:acs /var/log/jenkins
 chown -R manager:acs /var/cache/jenkins
