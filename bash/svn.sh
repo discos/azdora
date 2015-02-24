00df667a540a00c1b60086a552c1a73c4603720b
Marco Bartolini <marco.bartolini@gmail.com>
2015-02-24 15:44:38 +0100

    added configuration for SVNUSER

--- bash/svn.sh
+++ bash/svn.sh
@@ -11,8 +11,8 @@ mkdir -p $SVNROOT
 chown -R manager:acs /data
 chown -R manager:acs $SVNROOT
 
-#echo "checking out Nuraghe and ESCS"
-#su - manager
-#cd $SVNROOT
-#svn co svn+ssh://nuraghe-devel.oa-cagliari.inaf.it/ACS .
+echo "checking out Nuraghe and ESCS"
+su - manager
+cd $SVNROOT
+svn co svn+ssh://${SVNUSER}@nuraghe-devel.oa-cagliari.inaf.it/ACS .
 
